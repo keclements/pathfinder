f6208cb7c6b814fbf75f9514ff28a252959b3555
keclements <kristin.clements@gmail.com>
2019-04-08 11:40:09 -0400

    test

--- pathfinder.py
+++ pathfinder.py
@@ -1,1 +1,11 @@
+from PIL import image, ImageDraw
+
+class Map:
+
+	def __init__(self, filename):
+		self.elevations = self.get_elevations(filename)
+        self.max_elevation = self.get_max_elevation()
+        self.min_elevation = self.get_min_elevation()
+		with open(filename) as file:
+			self.elevations = [[int(e) for 
 
